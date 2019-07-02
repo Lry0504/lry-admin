839942a95b2d71928dc4e23ac88d4e4bac73fc38
Lry0504 <lry885175073@163.com>
2019-07-02 16:27:51 +0800

    添加导航路由

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import App from '../App'
+import Admin from '../admin'
+import Home from '../pages/Home'
+import Buttons from '../pages/UI/button'
 export default class RouterMap extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Router>
-          <Route exact path="/" component={Home} >
-            <Route path="/info_confirm" component={InfoConfirm} />
-            <Route path="/setting" component={Setting} />
-            <Route path="/tracking" component={tracking} />
-          </Route>
-        </Router>
-      </React.Fragment>
+      <BrowserRouter>
+        <App>
+          <Switch>
+            <Route path="/" render={() =>
+              <Admin>
+                <Switch>
+                  <Route path="/home" component={Home} />
+                  <Route path="/ui/buttons" component={Buttons} />
+                  <Redirect to="/home" />
+                </Switch>
+              </Admin>
+            } />
+          </Switch>
+        </App>
+      </BrowserRouter>
     )
   }
 }
