b6d1d820a9238127ad963e38edf17b441345175d
Lry0504 <lry885175073@163.com>
2019-07-02 21:16:01 +0800

    新增modals和loadings路由

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,8 @@ import App from '../App'
 import Admin from '../admin'
 import Home from '../pages/Home'
 import Buttons from '../pages/UI/button'
+import Modals from '../pages/UI/modal'
+import Loadings from '../pages/UI/loading'
 export default class RouterMap extends React.Component {
   render() {
     return (
@@ -15,6 +17,8 @@ export default class RouterMap extends React.Component {
                 <Switch>
                   <Route path="/home" component={Home} />
                   <Route path="/ui/buttons" component={Buttons} />
+                  <Route path="/ui/modals" component={Modals} />
+                  <Route path="/ui/loadings" component={Loadings} />
                   <Redirect to="/home" />
                 </Switch>
               </Admin>
