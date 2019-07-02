b3498f24072bd9db830ebb813c573b6f975867d5
Lry0504 <lry885175073@163.com>
2019-07-02 16:26:24 +0800

    时间戳处理

--- src/utils/formateDate.js
+++ src/utils/formateDate.js
@@ -2,6 +2,17 @@ export default {
   formateDate(time) {
     if(!time) return;
     let date = new Date(time);
-    return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    month = month < 10 ? "0"+month : month;
+    let day = date.getDate();
+    day = day < 10 ? "0"+day : day;
+    let hours = date.getHours();
+    hours = hours < 10 ? "0"+hours : hours;
+    let minutes = date.getMinutes();
+    minutes = minutes < 10 ? "0"+minutes : minutes;
+    let seconds = date.getSeconds();
+    seconds = seconds < 10 ? "0"+seconds : seconds;
+    return year +"-"+ month +"-"+ day +" "+ hours +":"+ minutes +":"+seconds;
   }
 }
