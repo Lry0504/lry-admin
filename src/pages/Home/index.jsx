d9f45047677dcf0c9d8bbf2b63a2157e7acb527d
Lry0504 <lry885175073@163.com>
2019-07-02 16:30:19 +0800

    Home首页信息

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.scss'
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-wrap">欢迎来到lry-admin后台管理系统</div>
+    )
+  }
+}
+
+export default Home
